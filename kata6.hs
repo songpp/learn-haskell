392c37df742345694e8322c7b5ddf70a54979f99
SongPeng <sun.april.moon@gmail.com>
2014-03-09 19:50:02 +0800

    kata6 find Anagrams  haskell version

---
+++ kata6.hs
@@ -0,0 +1,45 @@
+module Anagrams where
+
+import           Data.Char          (ord, toLower)
+import           Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as IntMap
+import           Data.List          (sortBy)
+import           System.Environment
+import           Text.Printf        (printf)
+
+main = do
+    args <- getArgs
+    let (filename : _) = args
+    contents <- readFile filename
+    let result = analyze . map (map toLower) . lines $ contents
+    let r = take 10 . sortBy sortA $ IntMap.elems result
+    print r
+
+type Word = String
+data A = A { size:: Int, ws::[String] }
+instance Show A where
+    show (A size words) = printf "%d => %s\n" size (show words)
+
+sortA a1 a2 =  size a2 `compare` size a1
+
+analyze :: [String] -> IntMap A
+analyze = foldl insert IntMap.empty . map wordHash
+    where
+        wordHash w = (,) (hashWords w) w
+        insert :: IntMap A -> (Int,String) -> IntMap A
+        insert acc (hash, word) = IntMap.insertWith increment hash (A 1 [word]) acc
+        increment (A s1 w1) (A s2 w2) = A (s1 + s2) (w1 ++ w2)
+
+
+hashWords :: String -> Int
+hashWords = product . map factors
+    where
+        factors = (hashFactors IntMap.!) . ord
+
+
+hashFactors = IntMap.fromList $ zip letters primes
+    where
+        letters = map ord (['a'..'z'] ++ "'-" ++ ['0'..'9'])
+        primes = [ x | x <- [2 .. ], isPrime x]
+        isPrime n = not $ any (\t -> n `rem` t == 0)
+                        [2 ..  floor . sqrt . fromIntegral $ n]
